08bc5de760f742b47ff0eb2910babc0e893b4d60
jaja-git <alexandre@flowkey.com>
2021-01-19 11:30:57 +0100

    exp result in data quality checks

--- 4-Data-Pipelines-with-Airflow/plugins/operators/data_quality.py
+++ 4-Data-Pipelines-with-Airflow/plugins/operators/data_quality.py
@@ -2,16 +2,14 @@ from airflow.hooks.postgres_hook import PostgresHook
 from airflow.models import BaseOperator
 from airflow.utils.decorators import apply_defaults
 
-from plugins.helpers.sql_queries import SqlQueries
-
 
 class DataQualityOperator(BaseOperator):
 
     check_dict = {
-        'songs': {'table': 'songs', 'col': 'title'},
-        'users': {'table': 'users', 'col': 'last_name'},
-        'artists': {'table': 'artists', 'col': 'name'},
-        'songplay': {'table': 'songplays', 'col': 'user_id'}
+        'songs': {'table':'songs', 'col':'title', 'expected_nulls': 0},
+        'users': {'table':'users', 'col':'last_name', 'expected_nulls': 0},
+        'artists': {'table':'artists', 'col':'name', 'expected_nulls': 0},
+        'songplay': {'table':'songplays', 'col':'user_id', 'expected_nulls': 0}
         }
 
     ui_color = '#89DA59'
@@ -33,7 +31,7 @@ class DataQualityOperator(BaseOperator):
     def execute(self, context, *args, **kwargs):
         redshift = PostgresHook(postgres_conn_id=self.redshift_conn_id)
         for key, value in self.checks.items():
-            records = redshift.get_records(f"SELECT COUNT(*) FROM {value['table']}") #[(604,)]
+            records = redshift.get_records(f"SELECT COUNT(*) FROM {value['table']}")
             if len(records) < 1 or len(records[0]) < 1:
                 raise ValueError(f"Data quality check failed. {value['table']} returned no results")
             num_records = records[0][0]
@@ -42,6 +40,6 @@ class DataQualityOperator(BaseOperator):
             self.log.info(f"Data quality check on table {value['table']} passed with {records[0][0]} records")
             null_records = redshift.get_records(f"SELECT COUNT(*) FROM {value['table']} where {value['col']} is null")
             num_null_records = null_records[0][0]
-            if num_null_records != 0:
+            if num_null_records != value['expected_nulls']:
                 raise ValueError(f"Data quality check failed. {value['table']} contained {null_records[0][0]} null {value['col']}")
             self.log.info(f"Data quality check on table {value['table']} passed with {num_null_records} null {value['col']}")
